463fe776716fd7ffa5c10ca68dd020cbc8faa154
Gaurav Ramesh <gggauravr@gmail.com>
2014-09-29 06:26:57 -0400

    improved volcano plot with legends

--- bmi/lab1.R
+++ bmi/lab1.R
@@ -13,6 +13,7 @@ library(affyPLM)
 library(limma)
 library(hgu133plus2.db)
 library(annotate)
+library(ggplot2)
 
 # define constants needed for the script
 constDataset <- "GSE10245"
@@ -97,63 +98,31 @@ write.table(gene.results, file="gene_details.txt", sep="\t")
 # write them to a file for convenience
 gene.up <- head(gene.results[gene.results$logFC > 0, ], n=10)
 gene.down <- head(gene.results[gene.results$logFC < 0, ], n=10)
-write.table(gene.up, file="genes_up_regulated.txt", sep="\t")
-write.table(gene.down, file="genes_down_regulated.txt", sep="\t")
+write.table(gene.up, file="gene_up.txt", sep="\t")
+write.table(gene.down, file="gene_down.txt", sep="\t")
 
-par(mar=c(3,3,2,1), mgp=c(2,.7,0), tck=-.01)
-plot(
-    # logFC on x- and -log p-value on y-axis
-    gene.list$logFC, 
-    -log10(gene.list$P.Value),
-     
-    # set x and y-axis limits
-    xlim=c(-10, 10), 
-    ylim=c(0, 15),
-
-    # set x- and y-axis labels
-    xlab="log2 fold change", ylab="-log10 p-value")
-
-
-sum(abs(gene.list$logFC) > 2 & gene.list$P.Value < 0.001)
-
-##no_of_genes=27,306
-no_of_genes = dim(probeset.list)[1]
-##Gives 693 genes
-sum(abs(gene.list$logFC) > 2 & gene.list$P.Value < 0.05/no_of_genes)
-
-install.packages("ggplot2")
-require(ggplot2)
-
-
-
-##Highlight genes that have an absolute fold change > 2 and a p-value < Bonferroni cut-off
-gene.list$threshold = as.factor(abs(gene.list$logFC) > 2 & gene.list$P.Value < 0.05/no_of_genes)
-
-##Construct the plot object
-g = ggplot(data=gene.list, aes(x=lproogFC, y=-log10(P.Value), colour=threshold)) + 
-  geom_point(alpha=0.4, size=1.75) + theme(legend.position = "none") + xlim(c(-10, 10)) + ylim(c(0, 15)) + xlab("log2 fold change") + ylab("-log10 p-value")
-g
-
-ind <- which(gene.list$gene.symbols == 'ESR1')
-##Graph not shown
-##g + geom_text(aes(x=gene.list[ind[1],]$logFC, y=-log10(gene.list[ind[1],]$P.Value), label=gene.list[ind[1],]$gene.symbols, size=1.2), colour="black")
-g + geom_text(aes(x=gene.list[ind,]$logFC, y=-log10(gene.list[ind,]$P.Value), label=gene.list[ind,]$gene.symbols, size=1.2), colour="black")
-
-gene.symbols[is.na(gene.symbols)] <- "XXXXX"
+# convert NA values to symbols for consistent handling
+gene.symbols[is.na(gene.symbols)] <- "????"
 probe.matches <- gene.symbols == "EGFR" | gene.symbols == "SOX2" | gene.symbols == "TP53"
 probe.not.positions <- which(!probe.matches)
-probe.labels <- gene.symbols
+
+# construct a better indicative vector for labels
+# label format: "(probe_id, gene_symbol)"
+probe.labels <- paste("(", row.names(gene.results), ", ", gene.symbols, ")", sep="")
+# set unwanted probe labels to NA, for elimination
 probe.labels[probe.not.positions] <- NA
-# probe.labels <-  gene.symbols[probe.positions]
-
+write.table(probe.labels[!is.na(probe.labels)], file="gene_probe_map.txt")
 ggplot(gene.list, 
-        aes(
-         label=probe.labels,
-         x=gene.list$logFC, 
-         y=-log10(gene.list$P.Value) )) + 
-        geom_point(aes(shape=probe.matches, color=probe.matches)) + 
-        geom_text(size=3) +
-        xlab("log2 fold-change") + 
-        ylab("-log10 p-value")
-
+  aes(
+     label=probe.labels,
+     x=gene.list$logFC, 
+     y=-log10(gene.list$P.Value))) + 
+  geom_point(aes(shape=probe.matches, 
+                 colour=probe.matches)) + 
+  geom_text(size=3) + 
+  xlab("Log2 Fold-Change") + 
+  ylab("-Log10 P-Value") + 
+  scale_shape_discrete(name="Shape", breaks=c(TRUE, FALSE), labels=c("EGFR, TP53, SOX2", "Other")) +
+  scale_colour_discrete(name="Color", breaks=c(TRUE, FALSE), labels=c("EGFR, TP53, SOX2", "Other")) +
+  ggtitle("Volcano Plot(Fold-Change of Genes vs P-Value)")
 
